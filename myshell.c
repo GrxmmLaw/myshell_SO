1d0d87c908c6a89a058a45cf1ca38f0babac5ed3
GrxmmLaw <ivan.shinoda1308@gmail.com>
2025-09-13 22:02:02 -0300

    Soporte indeterminado de argumentos

--- myshell.c
+++ myshell.c
@@ -3,94 +3,104 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 
-#define MAX_CMDS 10
-#define MAX_ARGS 100
 
-void parse_line(char *line, char *cmds[MAX_CMDS]) {
-    int i = 0;
-    char *token = strtok(line, "|");
-    while (token && i < MAX_CMDS) {
-        while (*token == ' ') token++;
-        cmds[i++] = token;
-        token = strtok(NULL, "|");
-    }
-    cmds[i] = NULL;
+void print_prompt() {
+    printf("mishell:$ ");
+    fflush(stdout);
 }
 
-void parse_cmd(char *cmd, char *args[MAX_ARGS]) {
-    int i = 0;
-    char *token = strtok(cmd, " ");
-    while (token && i < MAX_ARGS - 1) {
-        args[i++] = token;
-        token = strtok(NULL, " ");
+int parse_line(char *line, char ***commands) {
+    int size = 8, count = 0;
+    *commands = malloc(size * sizeof(char*));
+    char *cmd = strtok(line, "|");
+    while (cmd != NULL) {
+        if (count >= size) {
+            size *= 2;
+            *commands = realloc(*commands, size * sizeof(char*));
+        }
+        (*commands)[count++] = cmd;
+        cmd = strtok(NULL, "|");
     }
-    args[i] = NULL;
+    return count;
+}
+
+void parse_args(char *command, char ***args, int *argc) {
+    int size = 8;
+    *args = malloc(size * sizeof(char*));
+    *argc = 0;
+    char *arg = strtok(command, " \t\n");
+    while (arg != NULL) {
+        if (*argc >= size) {
+            size *= 2;
+            *args = realloc(*args, size * sizeof(char*));
+        }
+        (*args)[(*argc)++] = arg;
+        arg = strtok(NULL, " \t\n");
+    }
+    (*args)[*argc] = NULL;
+}
+
+void execute_pipeline(char **commands, int num_cmds) {
+    int i, fd[2];
+    pid_t pid;
+    int prev_fd = -1;
+
+    for (i = 0; i < num_cmds; i++) {
+        char **args;
+        int argc;
+        parse_args(commands[i], &args, &argc);
+
+        if (i < num_cmds - 1) pipe(fd);
+
+        pid = fork();
+        if (pid == 0) {
+            if (i > 0) {
+                dup2(prev_fd, 0);
+                close(prev_fd);
+            }
+            if (i < num_cmds - 1) {
+                close(fd[0]);
+                dup2(fd[1], 1);
+                close(fd[1]);
+            }
+            execvp(args[0], args);
+            perror("Error al ejecutar comando");
+            exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+            perror("Error en fork");
+            return;
+        }
+        if (i > 0) close(prev_fd);
+        if (i < num_cmds - 1) {
+            close(fd[1]);
+            prev_fd = fd[0];
+        }
+        free(args); 
+    }
+    for (i = 0; i < num_cmds; i++) wait(NULL);
 }
 
 int main() {
     char *line = NULL;
     size_t len = 0;
+    char **commands;
 
     while (1) {
-        printf("mishell> ");
-        fflush(stdout);
-
-        ssize_t read = getline(&line, &len, stdin);
+        print_prompt();
+        ssize_t read = getline(&line, &len, stdin); 
         if (read == -1) break;
-
-        if (read == 1 || (line[0] == '\n')) continue;
-
-        if (line[read - 1] == '\n') line[read - 1] = '\0';
-
-        if (strcmp(line, "exit") == 0) break;
-
-        char *cmds[MAX_CMDS];
-        parse_line(line, cmds);
-
-        int num_cmds = 0;
-        while (cmds[num_cmds]) num_cmds++;
-
-        int pipefd[2 * (num_cmds - 1)];
-        for (int i = 0; i < num_cmds - 1; i++) {
-            if (pipe(pipefd + i*2) < 0) {
-                perror("pipe");
-                exit(EXIT_FAILURE);
-            }
+        if (line[0] == '\n') continue;
+        line[strcspn(line, "\n")] = 0;
+        int num_cmds = parse_line(line, &commands);
+        if (num_cmds == 1 && strcmp(commands[0], "exit") == 0) {
+            free(commands);
+            free(line);
+            exit(0);
         }
-
-        int status;
-        pid_t pid;
-        for (int i = 0; i < num_cmds; i++) {
-            pid = fork();
-            if (pid == 0) {
-                if (i > 0) {
-                    dup2(pipefd[(i-1)*2], 0); 
-                }
-                if (i < num_cmds - 1) {
-                    dup2(pipefd[i*2+1], 1); 
-                }
-                for (int j = 0; j < 2*(num_cmds-1); j++) close(pipefd[j]);
-
-                char *args[MAX_ARGS];
-                parse_cmd(cmds[i], args);
-
-   
-                execvp(args[0], args);
-
-                perror("comando no encontrado");
-                exit(EXIT_FAILURE);
-            } else if (pid < 0) {
-                perror("fork");
-                exit(EXIT_FAILURE);
-            }
-        }
-        
-     for (int j = 0; j < 2*(num_cmds-1); j++) close(pipefd[j]);
-        for (int i = 0; i < num_cmds; i++) waitpid(-1, &status, 0); 
+        execute_pipeline(commands, num_cmds);
+        free(commands);
     }
-
     free(line);
     return 0;
 }
